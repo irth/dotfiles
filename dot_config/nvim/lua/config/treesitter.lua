bb6d1886db820a93114b3044cfddc7be5d228e56
Wojciech Kwolek <wojciech@kwolek.io>
2023-06-25 00:40:01 +0200

    idk

--- dot_config/nvim/lua/config/treesitter.lua
+++ dot_config/nvim/lua/config/treesitter.lua
@@ -1,6 +1,6 @@
 return function()
   require('nvim-treesitter.configs').setup {
-    ensure_installed = { "terraform", "hcl", "nix" },
+    ensure_installed = { "terraform", "hcl", "nix", "html" },
     highlight = {
       enable = true,
     },
